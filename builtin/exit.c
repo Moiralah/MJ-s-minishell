dafebe2b0011e6079b5f6da0830e7bfa428f9b25
Moiralah <aira.idris@gmail.com>
2025-02-21 04:23:43 +0800

    update exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: huidris <huidris@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 22:33:03 by huidris           #+#    #+#             */
-/*   Updated: 2025/02/20 23:13:51 by huidris          ###   ########.fr       */
+/*   Updated: 2025/02/21 04:22:58 by huidris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,20 +36,19 @@ void	run_exit(int argc, char **argv, t_node *node)
 	if (argc >= 2 && legitnum(argv[1]))
 	{
 		node->exit_status = ft_atol(argv[1]);
-		if (node->exit_status < 0)
-			node->exit_status = 256 + (node->exit_status % 256);
 		if (argc > 2)
 		{
 			ft_putstr_fd("exit: too many arguments\n", 2);
 			return ;
 		}
-		exit(node->exit_status %= 256);
+		exit((unsigned char)node->exit_status);
 	}
 	else
 	{
 		ft_putstr_fd("exit: ", 2);
 		ft_putstr_fd(argv[1], 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
-		exit(node->exit_status = 2);
+		node->exit_status = 2;
+		exit(node->exit_status);
 	}
 }
