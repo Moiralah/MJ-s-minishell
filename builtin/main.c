7e57384b7e225f8c3a86032f78dbdb51d1cd4828
Moiralah <aira.idris@gmail.com>
2025-02-21 05:41:06 +0800

    add makefile for testing

---
+++ builtin/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: huidris <huidris@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 18:08:05 by huidris           #+#    #+#             */
+/*   Updated: 2025/02/21 05:27:01 by huidris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "builtin.h"
+
+int	main(void)
+{
+	//t_node *node;
+
+	//node = malloc(sizeof(t_node));
+	//node = (t_node *){0};
+	//node->env = init_env(envp);
+	//run_env(node->env);
+	//printf("Original PWD: %s\n", ft_getenv("PWD", node));
+	//printf("%s", getcwd(NULL, 0));
+	//run_cd(argc, argv, node);
+	//run_env(node->env);
+	//printf("New PWD:%s\n", ft_getenv("PWD", node));
+	//run_exit(argc, argv, node);
+	//printf("exit status: %d\n", node->exit_status);
+	//run_echo(argv);
+	//run_pwd();
+	//run_export(argv, node);
+	//run_env(node->env);
+	//run_unset(argv, node);
+	//run_env(node->env);
+	//free(node);
+	init_signal();
+
+}
